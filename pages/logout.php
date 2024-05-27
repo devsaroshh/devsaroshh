df7c1d128f3d6e64dd8f7dbad665f96d2f8785ca
moniza_irfan <moniza.irfan@devsinc.com>
2024-05-27 17:44:15 +0500

    logout changes

--- pages/logout.php
+++ pages/logout.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 session_destroy();
-header('Location: ../index.php');
+
+header("Cache-Control: no-cache, no-store, must-revalidate"); 
+header("Pragma: no-cache"); 
+header("Expires: 0"); 
+
+header('Location: ../index.php'); 
 exit();
 ?>
