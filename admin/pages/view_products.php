fa9f9b8bbc42923cf7410a30cd00800bba6e6b40
moniza_irfan <moniza.irfan@devsinc.com>
2024-05-31 19:35:53 +0500

    pos system

--- admin/pages/view_products.php
+++ admin/pages/view_products.php
@@ -61,7 +61,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_id'])) {
     exit();
 }
 
-// Pagination setup
 $products_per_page = 9;
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 $offset = ($page - 1) * $products_per_page;
